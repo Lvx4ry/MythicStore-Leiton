c3fdd966010c1fb0c1e0cf0a86fb046e8429e02a
Luxary <f.leiton001@gmail.com>
2023-01-08 18:30:36 -0300

    se agrega lista de productos

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -2,64 +2,68 @@ import "./styles.css";
 import CartWidget from "./CartWidget";
 import SearchBar from "./SearchBar";
 
-const Header = () => {
+/* const Header = () => {
   return (
     <header className="header">
       <NavBar />
     </header>
   );
-};
+}; */
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid justify-content-end">
-        <a
-          className="navbar-brand ps-2 text-light col-lg-3 col-12 text-center text-lg-start"
-          href="#"
-        >
-          Mythic Store
-        </a>
-        <SearchBar />
-        <button
-          className="navbar-toggler col-12 my-2"
-          type="button"
-          data-bs-toggle=""
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse d-flex pe-2 col-4 justify-content-start px-3 justify-content-lg-end "
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link text-light" href="#">
-                Ofertas
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-light" href="#">
-                Vender
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-light" href="#">
-                Ingresar
-              </a>
-            </li>
-            <li className="nav-item">
-              <CartWidget numberOfProducts={3} />
-            </li>
-          </ul>
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid justify-content-end">
+          <div className="col-lg-3 col-12">
+            <a
+              className="navbar-brand ps-2 text-light text-center text-lg-start"
+              href="#"
+            >
+              Mythic Store
+            </a>
+          </div>
+          <SearchBar />
+          <button
+            className="navbar-toggler col-12 my-2"
+            type="button"
+            data-bs-toggle=""
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse d-flex pe-2 col-4 justify-content-start px-3 justify-content-lg-end "
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Ofertas
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Vender
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Ingresar
+                </a>
+              </li>
+              <li className="nav-item">
+                <CartWidget numberOfProducts={3} />
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
-export default Header;
+export default NavBar;
